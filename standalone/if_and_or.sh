60a0b09b92ba9c8d93a31d5bd59e5a96d240f653
jaclu <jacob.lundqvist@gmail.com>
2025-10-06 13:39:33 +0200

    added short form if

--- standalone/if_and_or.sh
+++ standalone/if_and_or.sh
@@ -5,9 +5,15 @@ if [ -d /proc/ish ] && [ -n "$TERMUX_VERSION" ]; then
     echo "iSH and Termux - ok beyond unlikely"
 fi
 
+# short form condition
+[ -d /proc/ish ] && [ -n "$TERMUX_VERSION" ] && {
+    echo "iSH and Termux - ok beyond unlikely"
+}
+
 
 if [ -d /proc/ish ] || [ -n "$TERMUX_VERSION" ]; then
     echo "iSH or Termux"
 fi
 
 
+echo "Done!"
